976224b92c71f707b70ea1f5f9cda601fb24bafd
MrHazMat <rahmat.ahmadhafizuddin@gmail.com>
2022-09-02 21:45:23 +0800

    Proper crossbow code

--- src/main/java/mrhazmat/firelance/init/FireLanceRegisters.java
+++ src/main/java/mrhazmat/firelance/init/FireLanceRegisters.java
@@ -2,6 +2,7 @@ package mrhazmat.firelance.init;
 
 import mrhazmat.firelance.FireLance;
 import mrhazmat.firelance.item.FireLanceItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
@@ -10,5 +11,5 @@ import net.minecraft.world.item.Item;
 
 public class FireLanceRegisters {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, FireLance.MODID);
-	public static final RegistryObject<Item> FIRE_LANCE = REGISTRY.register("fire_lance", () -> new FireLanceItem());
+	public static final RegistryObject<Item> FIRE_LANCE = REGISTRY.register("fire_lance", () -> new FireLanceItem(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_COMBAT).durability(999)));
 }
